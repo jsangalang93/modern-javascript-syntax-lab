26005c96fad8cb28439da667197b1be8558a5eeb
jsangalang93 <jsangalang93@gmail.com>
2024-05-08 14:16:55 -0400

    exercise 7 wip

--- server.js
+++ server.js
@@ -13,3 +13,85 @@ const nums2 = nums.map((currentElement) => {
 });
 
 console.log(nums2);
+
+//EXERCISE 2
+
+// Given the following array, use destructuring to pull out the first and second values and place them into variables. Log both variables.
+
+// const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+
+// Your code here
+
+// const [firstTopping, secondTopping] = pizzaToppings;
+
+// console.log(firstTopping, secondTopping);
+// console.log(pizzaToppings[0], pizzaToppings[1]);
+
+// EXERCISE 3
+
+// Given the following object, use destructuring to create variables `make` and `model` that will hold the respective values.
+
+// const car = {
+//     make: 'Audi',
+//     model: 'q5',
+//   };
+  
+  // Your code here
+
+// const { make, model } = car;
+
+//   console.log(make, model);
+
+//EXERCISE 4
+
+// Duplicate the following array using the spread operator and assign it to `controversialPizzaToppings`. Then, log the variable.
+
+const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+
+// Your code here
+
+const controversialPizzaToppings = [...pizzaToppings];
+  console.log(controversialPizzaToppings);
+  
+// EXERCISE 5
+
+// Duplicate the following object and spread its values into a new variable `myCar`.
+
+const car = {
+    make: 'Audi',
+    model: 'q5',
+  };
+  
+  // Change the `model` property of `myCar` to 'q7'. Log both objects.
+  
+  // Your code here
+const myCar = {...car};
+
+//   const myCar = car;
+  myCar.model = 'q7';
+
+  console.log('Original:', car);
+  console.log('Clone:', myCar);
+
+// EXERCISE 6
+
+// Create an object named userProfile. 
+// Define a variable named propertyName and assign a string to it (like a username, age, or email). 
+// Use propertyName as a dynamic key in userProfile, assigning a relevant value.
+
+// Your code here
+const userProfile = 'age';
+const propertyName = {
+    [userProfile]: 31,
+};
+
+console.log(propertyName);
+
+// EXERCISE 7 [null];
+
+//EXERCISE 8
+
+
+    
+
+    
